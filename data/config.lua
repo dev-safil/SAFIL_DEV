dd05b8bfe7b1198e985d2d7624a40f6f5768dd26
dev-safil <yrkt47k5b@disbox.org>
2017-04-02 11:10:41 +0300

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,22 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "banhammer",
+    "groupmanager",
+    "msg-checks",
+    "plugins",
+    "tools",
+    "me",
+    "addreplay"
+  },
+  info_text = "📌¦ Welcome To My Dear\n\nTH3BOSS V12 \nFor More Information Subscribe To The Channel @llDEV1ll \n https://github.com/moody2020/TH3BOSS\n\n📌¦ Dev @TH3BOSS\n\n📌¦ Dev Bot @ll60Kllbot\n\n📌¦ Channel @llDEV1ll ",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    226861861
+  }
+}
+return _
+end
